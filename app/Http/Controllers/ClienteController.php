62a3b14719c7292ff048f7a6e4eac60fe40c345b
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2024-05-12 01:14:07 -0300

    Versão Inicial API

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    public function create(Request $request)
+    {
+        $cliente = Cliente::create($request->all());
+        return response()->json($cliente, 201);
+    }
+
+    public function recover($id)
+    {
+        $cliente = Cliente::find($id);
+        if (!$cliente) {
+            return response()->json(['message' => 'Cliente nao encontrado'], 404);
+        }
+        return response()->json($cliente);
+    }
+
+    public function update(Request $request)
+    {
+        $cliente = Cliente::find($request->id);
+        if (!$cliente) {
+            return response()->json(['message' => 'Cliente nao encontrado'], 404);
+        }
+        $cliente->update($request->all());
+        return response()->json($cliente);
+    }
+
+    public function delete($id)
+    {
+        $cliente = Cliente::find($id);
+        if (!$cliente) {
+            return response()->json(['message' => 'Cliente nao encontrado'], 404);
+        }
+        $cliente->delete();
+        return response()->json(['message' => 'Cliente deletado com sucesso']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/cliente/create', [ClienteController::class, 'create']);
+Route::get('/cliente/{id}', [ClienteController::class, 'recover']);
+Route::put('/cliente/update', [ClienteController::class, 'update']);
+Route::delete('/cliente/{id}', [ClienteController::class, 'delete']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
