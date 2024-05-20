6fea1a1a6e9f4f6996c12962884798ab5ee6a609
xRokus <rafael_cintra_@hotmail.com>
2024-05-20 13:39:19 -0300

    Versão com criptografia ao popular na tabela

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -4,19 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Cliente;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class ClienteController extends Controller
 {
-
     public function index()
     {
-        $clientes = Cliente::all();
+        $clientes = Cliente::all()->map(function ($cliente) {
+            $cliente->nome = Crypt::decryptString($cliente->nome);
+            $cliente->email = Crypt::decryptString($cliente->email);
+
+            return $cliente;
+        });
+
         return response()->json($clientes);
     }
 
     public function create(Request $request)
     {
-        $cliente = Cliente::create($request->all());
+        $cliente = new Cliente;
+        $cliente->nome = Crypt::encryptString($request->input('nome'));
+        $cliente->email = Crypt::encryptString($request->input('email'));
+
+        $cliente->save();
+
+        $cliente->nome = $request->input('nome');
+        $cliente->email = $request->input('email');
+
         return response()->json($cliente, 201);
     }
 
@@ -24,8 +38,11 @@ class ClienteController extends Controller
     {
         $cliente = Cliente::find($id);
         if (!$cliente) {
-            return response()->json(['message' => 'Cliente nao encontrado'], 404);
+            return response()->json(['message' => 'Cliente não encontrado'], 404);
         }
+        $cliente->nome = Crypt::decryptString($cliente->nome);
+        $cliente->email = Crypt::decryptString($cliente->email);
+
         return response()->json($cliente);
     }
 
@@ -33,9 +50,24 @@ class ClienteController extends Controller
     {
         $cliente = Cliente::find($request->id);
         if (!$cliente) {
-            return response()->json(['message' => 'Cliente nao encontrado'], 404);
+            return response()->json(['message' => 'Cliente não encontrado'], 404);
         }
-        $cliente->update($request->all());
+        if ($request->has('nome')) {
+            $cliente->nome = Crypt::encryptString($request->input('nome'));
+        }
+        if ($request->has('email')) {
+            $cliente->email = Crypt::encryptString($request->input('email'));
+        }
+
+        $cliente->save();
+
+        if ($request->has('nome')) {
+            $cliente->nome = $request->input('nome');
+        }
+        if ($request->has('email')) {
+            $cliente->email = $request->input('email');
+        }
+
         return response()->json($cliente);
     }
 
@@ -43,7 +75,7 @@ class ClienteController extends Controller
     {
         $cliente = Cliente::find($id);
         if (!$cliente) {
-            return response()->json(['message' => 'Cliente nao encontrado'], 404);
+            return response()->json(['message' => 'Cliente não encontrado'], 404);
         }
         $cliente->delete();
         return response()->json(['message' => 'Cliente deletado com sucesso']);
